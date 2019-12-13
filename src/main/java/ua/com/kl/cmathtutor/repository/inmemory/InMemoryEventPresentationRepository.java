a5ba0452dd5d0182fb83b199220c5b647398e893
Hryhorii Popov <hryhorii_popov@epam.com>
2019-12-13 20:36:38 +0200

    Refactor event presentation repository
    Create new AbstractRefreshableCrudInMemoryRepository class to manage
    entities, which needs to be refreshed after being fetched.

--- src/main/java/ua/com/kl/cmathtutor/repository/inmemory/InMemoryEventPresentationRepository.java
+++ src/main/java/ua/com/kl/cmathtutor/repository/inmemory/InMemoryEventPresentationRepository.java
@@ -1,9 +1,6 @@
 package ua.com.kl.cmathtutor.repository.inmemory;
 
-import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 import ua.com.kl.cmathtutor.domain.entity.Event;
 import ua.com.kl.cmathtutor.domain.entity.EventPresentation;
@@ -11,7 +8,7 @@ import ua.com.kl.cmathtutor.exception.MandatoryAttributeException;
 import ua.com.kl.cmathtutor.repository.EventPresentationRepository;
 import ua.com.kl.cmathtutor.repository.EventRepository;
 
-public class InMemoryEventPresentationRepository extends AbstractCrudInMemoryRepository<EventPresentation>
+public class InMemoryEventPresentationRepository extends AbstractRefreshableCrudInMemoryRepository<EventPresentation>
 	implements EventPresentationRepository {
 
     private EventRepository eventRepository;
@@ -21,30 +18,21 @@ public class InMemoryEventPresentationRepository extends AbstractCrudInMemoryRep
     }
 
     @Override
-    public EventPresentation save(EventPresentation entity) {
-	if (Objects.isNull(entity.getEvent())) {
+    public EventPresentation save(EventPresentation eventPresentation) {
+	if (Objects.isNull(eventPresentation.getEvent())) {
 	    throw new MandatoryAttributeException("EventPresentation must have reference to Event!");
 	}
-	if (Objects.isNull(entity.getAuditorium())) {
+	if (Objects.isNull(eventPresentation.getAuditorium())) {
 	    throw new MandatoryAttributeException("EventPresentation must have reference to Auditorium!");
 	}
-	entity.setEvent(eventRepository.save(entity.getEvent()));
-	return super.save(entity);
+	eventPresentation.setEvent(eventRepository.save(eventPresentation.getEvent()));
+	return super.save(eventPresentation);
     }
 
     @Override
-    public Optional<EventPresentation> findById(Integer id) {
-	return super.findById(id).map(this::refreshEvent);
-    }
-
-    private EventPresentation refreshEvent(EventPresentation eventPresentation) {
+    protected EventPresentation refresh(EventPresentation eventPresentation) {
 	Event event = eventRepository.findById(eventPresentation.getEvent().getId()).get();
 	eventPresentation.setEvent(event);
 	return eventPresentation;
-    }
-
-    @Override
-    public List<EventPresentation> findAll() {
-	return super.findAll().stream().map(this::refreshEvent).collect(Collectors.toList());
     }
 }

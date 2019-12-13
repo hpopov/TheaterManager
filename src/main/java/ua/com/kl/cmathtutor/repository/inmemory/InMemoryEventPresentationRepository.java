4777f98c1e848a6dbf889a931ac88d387db25f4f
Hryhorii Popov <hryhorii_popov@epam.com>
2019-12-13 21:00:41 +0200

    Spring injection for repositories is performed using properties

--- src/main/java/ua/com/kl/cmathtutor/repository/inmemory/InMemoryEventPresentationRepository.java
+++ src/main/java/ua/com/kl/cmathtutor/repository/inmemory/InMemoryEventPresentationRepository.java
@@ -2,6 +2,7 @@ package ua.com.kl.cmathtutor.repository.inmemory;
 
 import java.util.Objects;
 
+import lombok.Setter;
 import ua.com.kl.cmathtutor.domain.entity.Event;
 import ua.com.kl.cmathtutor.domain.entity.EventPresentation;
 import ua.com.kl.cmathtutor.exception.MandatoryAttributeException;
@@ -11,11 +12,8 @@ import ua.com.kl.cmathtutor.repository.EventRepository;
 public class InMemoryEventPresentationRepository extends AbstractRefreshableCrudInMemoryRepository<EventPresentation>
 	implements EventPresentationRepository {
 
+    @Setter
     private EventRepository eventRepository;
-
-    private InMemoryEventPresentationRepository(EventRepository eventRepository) {
-	this.eventRepository = eventRepository;
-    }
 
     @Override
     public EventPresentation save(EventPresentation eventPresentation) {
